ee590df3e95f2a939ea8b33a61e79a9684c0c078
YeungWanLum <295386487@qq.com>
2022-06-21 22:20:07 +0800

    :wrench: 更新文档配置文件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -94,8 +94,9 @@ function sidebarScaffold() {
       text: '脚手架',
       collapsible: true,
       items: [
-        { text: '[vite]快速入门', link: '/scaffold/vite-quickstart' },
         { text: '基本介绍', link: '/scaffold/introduction' },
+        { text: '[vite]快速入门', link: '/scaffold/vite-quickstart' },
+       
       ]
     }
   ]
