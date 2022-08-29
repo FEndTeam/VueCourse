53ee2e0c1fedff43d43abf35ea36458b29488625
YeungWanLum <295386487@qq.com>
2022-08-29 22:16:45 +0800

    update: 在md文件中写vue

---
+++ docs/components/AppHeader.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+import { ref } from "vue";
+const num = ref(0);
+const setNum = ()=>{
+  num.value = 10000;
+}
+</script>
+<template>
+  <div>sdfasfsdf{{ num }}</div>
+  <button @click="setNum" >点击我看看</button>
+</template>
+
+
+
--- docs/state/introduction.md
+++ docs/state/introduction.md
@@ -1,1 +1,15 @@
 # pinia简介
+
+<script setup>
+import AppHeader from '../components/AppHeader.vue'
+</script>
+
+# Docs
+
+This is a .md using a custom component
+
+<AppHeader />
+
+## More docs
+
+...
